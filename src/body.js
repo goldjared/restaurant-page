1e2fbe38b72e837480799fd816b5e2dd83ccd60b
jared <jaredgold@proton.me>
2023-03-11 13:12:28 -0500

    build site foundations and initial stylesheet

--- src/body.js
+++ src/body.js
@@ -2,17 +2,23 @@
 //     <img src="" alt="head chef">
 //     <div class="text">Enidras is more.</div>
 //   </main> */}
-
+import image from './askelad.jpg';
 
 
 function pageBody(){
   // const content = document.querySelector('#content');
   const main = document.createElement('main');
   const img = document.createElement('img');
-  img.src = './askelad.jpg'
+  img.src = image;
+
+  const caption = document.createElement('div');
+  caption.classList.add('text');
+  caption.textContent = 'Enidras is more.';
+  
 
   content.append(main);
   main.append(img);
+  main.append(caption);
 }
 
 export {pageBody};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import { pageHead } from "./header";
 import { pageBody } from "./body";
+import { pageFoot } from "./footer";
 import './style.css';
+
+
 const content = document.querySelector('#content');
 // console.log('hello');
 pageHead();
 pageBody();
+pageFoot();
 
 
 
