526d1a06e91b360b071c9532320bc4c9f250f5e1
jared <jaredgold@proton.me>
2023-03-11 13:12:45 -0500

    update webpack with watch and css

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,16 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
 
       },
+      {
+
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: 'asset/resource',
+
+      },
 
     ],
 
   },
+  
 };
