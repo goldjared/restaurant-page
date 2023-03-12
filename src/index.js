0769f3da8678a12ff3cc47612a1ed8fdd14ec68d
jared <jaredgold@proton.me>
2023-03-12 12:34:23 -0400

    remove mistype

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
+import { pageLoad } from "./loader";
 import { pageHead } from "./header";
 import { pageBody } from "./body";
 import { pageFoot } from "./footer";
 import './style.css';
 
-
 const content = document.querySelector('#content');
+
+pageLoad();
 // console.log('hello');
-pageHead();
-pageBody();
-pageFoot();
+// pageHead();
+// pageBody();
+// pageFoot();
+
+const pages = document.querySelector('.header-options');
+
+pages.addEventListener('click', (e) => {
+  e.preventDefault();
+  pageLoad(e.target.innerText);
+})
+
+
 
 
 
