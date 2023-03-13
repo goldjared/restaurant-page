e0a1fcf0a065793b0ab4a0e9a1ebea832b54a073
jared <jaredgold@proton.me>
2023-03-13 15:37:58 -0400

    remove footer.js, combine into header.js, 2 sep files not needed

--- src/header.js
+++ src/header.js
@@ -40,6 +40,17 @@ function pageHead(){
   menu.append(menuLink);
   order.append(orderLink);
   contact.append(contactLink);
+
+  const footer = document.createElement('footer');
+  const footerGitHub = document.createElement('div');
+  footerGitHub.classList.add('footer-github');
+  const gitLink = document.createElement('a');
+  gitLink.setAttribute('href', 'linkurl');
+  gitLink.textContent = 'goldjared';
+
+  content.append(footer);
+  footer.append(footerGitHub);
+  footerGitHub.append(gitLink);
 }
 
 export {pageHead};
