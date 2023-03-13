628294b47184a6f19bd0a0391d71cf6697ddb2a1
jared <jaredgold@proton.me>
2023-03-13 17:31:19 -0400

    remove unused elements, update dist

--- src/header.js
+++ src/header.js
@@ -1,7 +1,4 @@
-
-
 function pageHead(){
-  // const content = document.querySelector('#content');
   const header = document.createElement('header');
   const headerName = document.createElement('div');
   headerName.classList.add('header-name');
@@ -12,7 +9,6 @@ function pageHead(){
   header.append(headerName);
   headerName.append(headerImg);
 
-
   const headerOptions = document.createElement('div');
   headerOptions.classList.add('header-options');
   header.append(headerOptions);
@@ -20,32 +16,22 @@ function pageHead(){
   const menu = document.createElement('div');
   menu.classList.add('menu');
   const menuLink = document.createElement('a');
-  // menuLink.setAttribute('href');
   menuLink.textContent = 'Menu';
 
   const order = document.createElement('div');
   order.classList.add('order');
   const orderLink = document.createElement('a');
-  // orderLink.setAttribute('href');
   orderLink.textContent = 'Order';
 
-  const contact = document.createElement('div');
-  contact.classList.add('contact');
-  const contactLink = document.createElement('a');
-  contactLink.setAttribute('href', 'linkurl');
-  contactLink.textContent = 'Contact';
-
-
-  headerOptions.append(menu, order, contact);
+  headerOptions.append(menu, order);
   menu.append(menuLink);
   order.append(orderLink);
-  contact.append(contactLink);
 
   const footer = document.createElement('footer');
   const footerGitHub = document.createElement('div');
   footerGitHub.classList.add('footer-github');
   const gitLink = document.createElement('a');
-  gitLink.setAttribute('href', 'linkurl');
+  gitLink.setAttribute('href', '');
   gitLink.textContent = 'goldjared';
 
   content.append(footer);
